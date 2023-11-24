cff8b9bfa4f069759a4593d9dadb57c4d5d55af7
arpan paudel <paudelarpan123@gmail.com>
2023-11-24 08:33:37 +0545

    fix: inserting patchbuiltin like basthon

--- jupyterlite_pyodide_kernel/__init__.py
+++ jupyterlite_pyodide_kernel/__init__.py
@@ -1,10 +1,12 @@
 """A JupyterLite kernel powered by Pyodide."""
 import builtins
 import js 
-
+from . import kernel as __kernel__
+from . import _patch_builtins
 from ._version import __version__
 
-__all__ = ["__version__", "_jupyter_labextension_paths","patch_input"]
+
+__all__ = ["__version__", "_jupyter_labextension_paths"]
 
 
 def _jupyter_labextension_paths():
@@ -12,23 +14,9 @@ def _jupyter_labextension_paths():
 
     return [{"src": "labextension", "dest": PYODIDE_KERNEL_NPM_NAME}]
 
-def patch_input():
-    """ Patching Pyodide input function and set toplevel async calls. """
-    _default_input = builtins.input
 
-    def _patched_input(prompt=None):
-        if prompt is not None:
-            print(prompt, end='', flush=True)
-        res = js.prompt(prompt)
-        print(res)
-        return res
+_patch_builtins.patch_all()
 
-    # copying all writable attributes (usefull to keep docstring and name)
-    for a in dir(_default_input):
-        try:
-            setattr(_patched_input, a, getattr(_default_input, a))
-        except Exception:
-            pass
 
-    # replacing
-    builtins.input = _patched_input
+# avoid polluting __dict__
+del _patch_builtins
---
+++ jupyterlite_pyodide_kernel/_patch_builtins.py
@@ -0,0 +1,76 @@
+import pydoc
+import builtins
+import sys
+import js
+from . import kernel
+
+
+__all__ = ['patch_all', 'patch_input', 'patch_help']
+
+
+def patch_input():
+    """ Patching Pyodide input function and set toplevel async calls. """
+    _default_input = builtins.input
+
+    def _patched_input(prompt=None):
+        if prompt is not None:
+            print(prompt, end='', flush=True)
+        res = js.prompt(prompt)
+        print(res)
+        return res
+
+    # copying all writable attributes (usefull to keep docstring and name)
+    for a in dir(_default_input):
+        try:
+            setattr(_patched_input, a, getattr(_default_input, a))
+        except Exception:
+            pass
+
+    # replacing
+    builtins.input = _patched_input
+
+
+def patch_sleep():
+    """
+    Patching time.sleep function for toplevel async calls.
+
+    It is not a builtin function but the patch put stuff in builtins.
+    """
+    async def sleep_async(*args, **kwargs):
+        import time
+        local_sleep = kernel.locals().get("sleep", time.sleep)
+        if local_sleep != time.sleep:
+            return local_sleep(*args, **kwargs)
+        return await kernel.sleep_async(*args, **kwargs)
+    builtins._basthon_sleep_async = sleep_async
+
+
+def patch_help():
+    """ Patching help function.
+
+    See pydoc.py in cpython:
+    https://github.com/python/cpython/blob/master/Lib/pydoc.py
+    It uses a class called ModuleScanner to list packages.
+    This class first looks at sys.builtin_module_names then in pkgutil.
+    We fake sys.builtin_module_names in order to get it right.
+    """
+    _default_help = pydoc.help
+
+    def _patched_help(*args, **kwargs):
+        backup = sys.builtin_module_names
+        to_add = kernel.list_basthon_modules()
+        sys.builtin_module_names = backup + tuple(to_add)
+        res = _default_help(*args, **kwargs)
+        sys.builtin_module_names = backup
+        return res
+
+    pydoc.help = _patched_help
+
+
+def patch_all():
+    """
+    Patch all builtins by calling all patch_* methods.
+    """
+    for name, method in globals().items():
+        if name.startswith('patch_') and name != 'patch_all':
+            method()
